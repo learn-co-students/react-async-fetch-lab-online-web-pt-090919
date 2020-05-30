799a396d5b52749fee2e698c8c8fb6a91312df35
Randy <randyisbusy@gmail.com>
2020-05-30 00:19:01 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, {Component} from 'react';
+
+export default class App extends Component{
+    constructor(props){
+        super(props) 
+        this.state = {people: [ {name : "Randy"} ]} 
+        this.url = 'http://api.open-notify.org/astros.json'
+    }
+    makeEles = () =>{  
+            let {people} =  this.state
+        return people.map(person => <h1>{person.name}</h1>)
+    }
+    componentDidMount(){  
+        console.log(this.url)
+        fetch(this.url) 
+        .then(res => res.json()) 
+        .then(data => this.setState({ people : data.people }))
+    }
+    
+
+    render(){
+        return (<div>{this.makeEles()}</div>)
+    }
+}
